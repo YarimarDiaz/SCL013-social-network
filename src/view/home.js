feb72b546ed5614120e9a57b9dc0298afbff1590
AnlianaAguilar <anlianaaguilar@gmail.com>
2020-06-15 13:00:23 -0400

    Cambios en mensaje de error, foto y usuario de google

--- src/view/home.js
+++ src/view/home.js
@@ -12,7 +12,7 @@ export const home = () => {
           <input class="login" id="txtEmail" type="email" name="user-email" placeholder="Direccion de correo">
           <input class="login" id="txtPasswword" type="password" name="user-contraseña" placeholder="Contraseña">
           <button class="login" id="btnLogin" type="button" name="singIn">Iniciar Sesion</Button>
-          <article class="login" class="article"></article>
+          <h3 id="mensajes"></h3>
           <input class="login" type="text" id="google" value="Iniciar sesion con Google">
           <p>¿No tienes cuenta? <a href='#/singIn'><button>Registrate</button></a></p>
         </div>
